4e2456a1430985713e270645c1dab05495eb2f61
Incredible111 <1239366535@qq.com>
2023-08-17 17:20:13 +0800

    Initial commit

---
+++ Incredible_blog/src/main/java/com/Incredible/IncredibleBlogApplication.java
@@ -0,0 +1,23 @@
+package com.Incredible;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Incredible
+ * @Date 2023/8/17 11:27
+ * @PackageName:com.Incredible
+ * @ClassName: IncredibleBlogApplication
+ * @Description: TODO
+ * @Version 1.0
+ */
+
+@MapperScan("com.Incredible.mapper")
+@SpringBootApplication
+public class IncredibleBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IncredibleBlogApplication.class,args);
+    }
+}
+
